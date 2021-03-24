6f4401058a53d551527cda9d7ebbb81d008b14b0
Saramag <saramagsaramag@yandex.ru>
2021-03-24 19:20:46 +0300

    Remove legenda

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests
+#import json
+import re
 
 S = requests.Session()
 URL = "https://ru.wikipedia.org/w/api.php"
@@ -48,10 +50,11 @@ def get_watchlist():
     return watchlist
 
 # Get contribs of %username%
-def get_user_contribs(username):
+def get_user_contribs(username, ucend):
     ucuser = username
+    ucendr = ucend
     usercontribs_params = {
-     "action": "query",
+        "action": "query",
         "format": "json",
         # Set username to watch
         "ucuser": ucuser,
@@ -60,8 +63,8 @@ def get_user_contribs(username):
         # All list = max
         "uclimit": "max",
         # From time
-        "ucend": "2021-03-21T09:47:17Z"
-        }
+        "ucend": ucendr
+    }
 
     # Take last contribs
     R = S.get(url=URL, params=usercontribs_params)
@@ -79,7 +82,57 @@ def get_user_contribs(username):
         #print(uc["title"])
         print(uc)
 
-#get_user_contribs("Saramag")
+#get_user_contribs("Saramag","2021-03-21T09:47:17Z")
+
+# Get settings to search
+# https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles=Pet_door&formatversion=2&rvprop=content&rvslots=*
+
+usercontribs_params = {
+        "action": "query",
+        "format": "json",
+        "prop": "revisions",
+        "titles": "Участник:Сэр Джордж Тейлор/Watch-list",
+        "formatversion": "2",
+        "rvprop": "content",
+        "rvslots": "*"
+    }
+
+r = S.get(url=URL, params=usercontribs_params)
+data = r.json()
+#test for get-data
+#error in coder
+#parsed_string = json.dumps(data)
+# Parse json to str
+parsed_string = data["query"]["pages"][0]["revisions"][0]["slots"]["main"]["content"]
+#
+#print(data)
+# Remove legenda
+parsed_string = parsed_string.split("):",1)[1]
+# Remove spaces before
+parsed_string = parsed_string.strip()
+mylist = re.split("'.+'gm", parsed_string)
+print(mylist)
+print(parsed_string)
+dict_with_data = {
+  "user": "test",
+  "date_start": "2021-03-21T00:00:00Z",
+  "date_stop": "2022-03-21T00:00:00Z"
+}
+print(dict_with_data)
+#for i in parsed_string:
+#    print(i)
+data_aray = [[ ['a' for col in range(3)] for col in range(3)] for row in range(4)]
+print(data_aray)
+#\n\nSaramag,
+#content = data["query"]["pages"]
+#content2 = content["revisions"]["slots"]
+#print(content)
+
+#if USERCONTRIBS[0]:
+#if USERCONTRIBS[0] == "":
+#print("No contribs for this data")
+
+
 
 
 
